bf295d906015157c85791e2179a5c8203a81547d
Hilkka Lammi <lammi.hilkka@gmail.com>
2016-02-08 18:31:01 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+jQuery(function($) {
+
+    //#main-slider
+    var slideHeight = $(window).height();
+    $('#home-slider .item').css('height',slideHeight);
+
+    $(window).resize(function(){'use strict',
+        $('#home-slider .item').css('height',slideHeight);
+                               });
+
+    //Scroll Menu
+    $(window).on('scroll', function(){
+        if( $(window).scrollTop()>slideHeight ){
+            $('.main-nav').addClass('navbar-fixed-top');
+        } else {
+            $('.main-nav').removeClass('navbar-fixed-top');
+        }
+    });
+
+    // Navigation Scroll
+    $(window).scroll(function(event) {
+        Scroll();
+    });
+
+    $('.navbar-collapse ul li a').on('click', function() {  
+        $('html, body').animate({scrollTop: $(this.hash).offset().top - 10}, 700);
+        return false;
+    });
+
+    // User define function
+    function Scroll() {
+        var contentTop      =   [];
+        var contentBottom   =   [];
+        var winTop      =   $(window).scrollTop();
+        var rangeTop    =   200;
+        var rangeBottom =   500;
+
+        $('.navbar-collapse').find('.scroll a').each(function(){
+            contentTop.push( $( $(this).attr('href') ).offset().top);
+            contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+        })
+        // Change active section
+        $.each( contentTop, function(i){
+            if ( winTop > contentTop[i] - rangeTop ){
+                $('.navbar-collapse li.scroll')
+                    .removeClass('active')
+                    .eq(i).addClass('active');			
+            }
+        })
+    };
+
+    $('#tohash').on('click', function(){
+        $('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+        return false;
+    });
+
+    //Initiat WOW JS
+    new WOW().init();
+    //smoothScroll
+    smoothScroll.init();
+
+    // Progress Bar
+    $('#my-skills').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuetransitiongoal')+'%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    $(".link").click(function(e){
+        e.preventDefault();
+        $(".popup").fadeIn(300,function(){$(this).focus();});
+    });
+
+    $('.close').click(function() {
+        $(".popup").fadeOut(300);
+    });
+    $(".popup").on('blur',function(){
+        $(this).fadeOut(300);
+    });
+
+
+});
